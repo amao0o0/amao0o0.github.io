0590d60da77c4213addaebf8354eb469ad4e8188
魏承威 <chengwei@usc-guestwireless-upc-new178.usc.edu>
2022-06-24 14:24:32 -0700

    initial commit

---
+++ jemdoc/compile.sh
@@ -0,0 +1,16 @@
+
+
+
+python2 jemdoc.py -o ../ index.jemdoc
+
+python2 jemdoc.py -o ../ research.jemdoc
+
+python2 jemdoc.py -o ../ publications.jemdoc
+
+python2 jemdoc.py -o ../ teaching.jemdoc
+
+python2 jemdoc.py -o ../ activities.jemdoc
+
+python2 jemdoc.py -o ../ biography.jemdoc
+
+#python2 jemdoc.py -o ../ statistics.jemdoc
